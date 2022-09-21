c5c7ea651095b16caef68c93ec14145d930e1d47
shagunsharma6677 <shagunsharma6677@gmail.com>
2022-09-21 08:32:55 +0530

    Day-3

---
+++ Day-2/Special_Pairs.js
@@ -0,0 +1,18 @@
+// a reconnaissance unit should consist of exactly two soldiers. Since these two soldiers shouldn't differ much, their heights can differ by at mostKcentimeters.
+
+
+count=0
+  for(let i=0; i<=a.length-1; i++){
+      
+      for(let j=0; j<=a.length-1; j++){
+          if(i==j) continue
+          else if(Math.abs(a[i]-a[j]) <=K){
+              count++
+          }
+  }
+  
+  
+ 
+  
+}
+console.log(count)
