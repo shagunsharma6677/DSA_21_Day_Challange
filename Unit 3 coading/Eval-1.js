1b4251bd0a268f122e02f264a662cb225d868f88
shagunsharma6677 <shagunsharma6677@gmail.com>
2022-10-12 22:35:11 +0530

    CF & Prototype

---
+++ Unit 3 coading/Eval-1.js
@@ -0,0 +1,12 @@
+// let person ={
+//     name: "Shagun",
+//     myName: function(){
+//         console.log(this.name)
+//     }
+// }
+// person.myName()
+
+// console.log("Hello")
+
+
+console.log("Hey")
